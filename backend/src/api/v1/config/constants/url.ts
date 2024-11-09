d5c101020200963658f363b0249453fcec979f81
royazizbarera <royazizbarera@gmail.com>
2024-11-10 00:26:55 +0700

    Add: modify url

--- backend/src/api/v1/config/constants/url.ts
+++ backend/src/api/v1/config/constants/url.ts
@@ -1,4 +1,6 @@
+const NODE_ENV: string = process.env.NODE_ENV || "production";
 
-
-
-export const FRONTEND_URL: string = 'http://localhost:3002';
+export const FRONTEND_URL: string =
+  NODE_ENV === "production"
+    ? "https://movie-nexus-jtk.vercel.app"
+    : "http://localhost:3002";
